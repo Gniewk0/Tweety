25f3e1669d290f5a7b7c7e05e4af6cd1dc606d7f
WojciechS <56968052+Gniewk0@users.noreply.github.com>
2020-05-19 22:14:25 +0200

    initial commit

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,15 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img 
+        src="https://picsum.photos/40" 
+        alt=""
+        class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sms">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae cupiditate quam explicabo minus, nostrum dignissimos in totam harum veritatis, voluptas reiciendis odio culpa dolore repellendus. Similique nihil cumque hic libero.
+        </p>
+    </div>
+</div>
